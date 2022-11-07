7bc3392a66f1e0de9c5a8a0a81e82d012267d822
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-07 11:18:33 -0800

    tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word=""):
+        self.word = word
+
+    def match(self, words_list):
+        matches = []
+
+        for word in words_list:
+            sorted_word = sorted([letter for letter in word])
+            sorted_init_word = sorted([word for word in self.word])
+            if sorted_word == sorted_init_word:
+                matches.append(word)
+
+        return matches
